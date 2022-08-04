2be7a5b90f77c06050137778a0ab19e1ffd74d30
samuelochaba <109516201+samuelochaba@users.noreply.github.com>
2022-08-04 09:40:16 +0100

    add code files

---
+++ script.js
@@ -0,0 +1,116 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let generateButton = document.querySelector("#gen-btn");
+let passwordContainerOne = document.querySelector("#password-1");
+let passwordContainerTwo = document.querySelector("#password-2");
+
+const generatePassword = () => {
+  let passwordArray = [];
+  let password = "";
+  for (let i = 0; i < 15; i++) {
+    passwordArray.push(Math.floor(Math.random() * characters.length));
+  }
+
+  passwordArray.forEach((item) => {
+    password += characters[item];
+  });
+
+  return password;
+};
+
+generateButton.addEventListener("click", () => {
+  passwordContainerOne.textContent = generatePassword();
+  passwordContainerTwo.textContent = generatePassword();
+});
